5f9291ce4bc2729cb4c19ac90a3d1fd7de76bbb3
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-14 14:15:18 +0000

    first commit

---
+++ lib/top_selling_nintendo_games.rb
@@ -0,0 +1,6 @@
+require "top_selling_nintendo_games/version"
+
+module TopSellingNintendoGames
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/top_selling_nintendo_games/version.rb
@@ -0,0 +1,3 @@
+module TopSellingNintendoGames
+  VERSION = "0.1.0"
+end

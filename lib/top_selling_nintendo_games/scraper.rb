b60ec1d3b000bff87c96fe7bfe7e8d49418264f9
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-15 22:02:19 +0000

    Finished Scraping attributes for first level

--- lib/top_selling_nintendo_games/scraper.rb
+++ lib/top_selling_nintendo_games/scraper.rb
@@ -5,15 +5,18 @@ class TopSellingNintendoGames::Scraper
     doc = Nokogiri::HTML(open(html))
     game_list =
     doc.css("ul.games").each {|game|
-      game.css("div.info").each {|attribute|
+
         binding.pry
+      game.css("div.info").each {|attribute|
+
         #Title: attribute.css("h3").text
         #release_date: attribute.css("p").first.children.last.text.strip
-        #
+        #price: attribute.css("p.b3.row-price").text
+        #console: attribute.css("p.b4").last.attr("data-system")
+        #url(2nd level): game.css("a").attr("href").text
       }
 
-      #title: doc.css("ul.games a").attr("data-game-title").value
-      #
+
     }
   end
 

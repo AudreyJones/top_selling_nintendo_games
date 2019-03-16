e6b42ff62edd83e8b746427a797c5fd2774db702
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-16 16:53:40 +0000

    encapsulated game title details

--- lib/top_selling_nintendo_games/scraper.rb
+++ lib/top_selling_nintendo_games/scraper.rb
@@ -15,15 +15,15 @@ class TopSellingNintendoGames::Scraper
         attributes[:console] = attribute.css("p.b4").last.attr("data-system")
       }
       open_url = Nokogiri::HTML(open(url))
-
+    game_details = {}
       open_url.css("article#game-detail").each {|title|
-      binding.pry
-      #Game Info: title.css("div.wrapper a")[0].attr("href")
-      # Add-on Content: title.css("div.wrapper a")[1].attr("href")
+        game_details[:game_info] = title.css("div.wrapper a")[0].attr("href")
+        game_details[:add_on_content] = title.css("div.wrapper a")[1].attr("href")
+        game_details[:reviews] = title.css("div.wrapper a")[2].attr("href")
+      # Game Info: title.css("div.wrapper a")[0].attr("href") #=> "#industry-quotes" (link)
+      # Add-on Content: title.css("div.wrapper a")[1].attr("href") #=> "#industry-quotes" (link)
       # Reviews/Industry Quotes: title.css("div.wrapper a")[2].attr("href") #=> "#industry-quotes" (link)
           }
     }
-
-
   end
 end

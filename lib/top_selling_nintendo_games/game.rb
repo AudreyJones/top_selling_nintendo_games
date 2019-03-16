fee2bc5e20083c715a7ca27cc14c988df110df32
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-16 18:51:12 +0000

    output seems to be communicating with scrape-filled object

--- lib/top_selling_nintendo_games/game.rb
+++ lib/top_selling_nintendo_games/game.rb
@@ -1,14 +1,11 @@
 class TopSellingNintendoGames::Game ##GAME
   attr_accessor :title, :release_date, :price, :console, :url
-  #Should encompass all behaviors and attributes/states of each game
+  attr_reader :all
   @@all = []
+  @@complete_games_collection = []
 
-  def initialize ## ALL Games created will have this done to them
+  def initialize
     @@all << self
-  end
-
-  def self.all
-    @@all
   end
 
   def self.create
@@ -16,21 +13,15 @@ class TopSellingNintendoGames::Game ##GAME
   end
 
   def self.new_from_site #This is your Custom Constructor:create games with properties from their title sites!
-
     new_game = self.create
     attributes = TopSellingNintendoGames::Scraper.scraper
-
-# New Game is populated with scraped data!
     attributes.each do |key,value|
       new_game.send("#{key}=",value)
     end
-binding.pry
     new_game
   end
 
-  def self.today
-    # self.new_from_site
+  def self.complete_games_collection
+    @@complete_games_collection << self.new_from_site
   end
-
-
 end
